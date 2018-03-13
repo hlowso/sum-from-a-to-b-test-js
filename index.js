747df2d174c593a1eeee85070d06b1b33885a65d
hlowso <h.canderleigh@gmail.com>
2018-03-13 16:44:57 +0000

    Done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if(fromN > toN) return 0;
+  if(fromN === toN) return fromN;
+
+  return fromN + sum(fromN + 1, toN - 1) + toN;
 }
 
 module.exports = sum;
